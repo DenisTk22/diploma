0214122ecdc84072bf52c301e18fb17afb2c02f8
Denis Tkachev <allpost-s@ya.ru>
2024-05-01 10:42:08 +0300

    nizhobl is started

--- scripts/solovki.js
+++ scripts/solovki.js
@@ -4,7 +4,7 @@ const solovkiTravels = `[
         "id": "1",
         "TravelName": "Соловки 2023",
         "Photo": "./images/solovki2023.jpg",
-        "Link": "#karelia__splavChirkaKem",
+        "Link": "#solovki__trip2023",
         "ShortDescription": "Остров в Белом море с тяжелой историей."
     }
 ]`
