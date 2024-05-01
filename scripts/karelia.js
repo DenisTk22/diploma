98f2e3e8fd308d48e7341b8daa2ab46b4c57e648
Denis Tkachev <allpost-s@ya.ru>
2024-05-01 22:35:23 +0300

    click

--- scripts/karelia.js
+++ scripts/karelia.js
@@ -18,46 +18,47 @@ const kareliaTravels = `[
 
 const kareliaTravelsData = JSON.parse(kareliaTravels);
 
+const kareliaMyTravelsEl = document.querySelector('.karelia__my-travels');
+
 kareliaTravelsData.forEach(travel => {
 
-    const kareliaMyTravelsEl = document.querySelector('.karelia__my-travels');
+    const travelEl = document.createElement('div');
+    travelEl.classList.add('karelia__travel');
+    kareliaMyTravelsEl.append(travelEl);
 
-        const travelEl = document.createElement('div');
-        travelEl.classList.add('karelia__travel');
-        kareliaMyTravelsEl.append(travelEl);
+    const travelNameEl = document.createElement('p');
+    travelNameEl.classList.add('karelia__travel_name');
+    travelNameEl.textContent = `${travel.TravelName}`;
+    travelEl.append(travelNameEl);
 
-        const travelNameEl = document.createElement('p');
-        travelNameEl.classList.add('karelia__travel_name');
-        travelNameEl.textContent = `${travel.TravelName}`;
-        travelEl.append(travelNameEl);
+    const travelLinkEl = document.createElement('a');
+    travelLinkEl.classList.add('karelia__travel_link');
+    travelLinkEl.href = `${travel.Link}`;
+    travelEl.append(travelLinkEl);
 
-        const travelLinkEl = document.createElement('a');
-        travelLinkEl.classList.add('karelia__travel_link');
-        travelLinkEl.href = `${travel.Link}`;
-        travelEl.append(travelLinkEl);
+    const travelPhotoEl = document.createElement('img');
+    travelPhotoEl.classList.add('karelia__travel_photo');
+    travelPhotoEl.src = travel.Photo;
+    travelPhotoEl.setAttribute('alt', `${travel.TravelName}`);
+    travelLinkEl.append(travelPhotoEl);
 
-            const travelPhotoEl = document.createElement('img');
-            travelPhotoEl.classList.add('karelia__travel_photo');
-            travelPhotoEl.src = travel.Photo;
-            travelPhotoEl.setAttribute('alt', `${travel.TravelName}`);
-            travelLinkEl.append(travelPhotoEl);
-
-        const shortDescriptionEl = document.createElement('p');
-        shortDescriptionEl.classList.add('karelia__travel_shortDescription');
-        shortDescriptionEl.textContent = travel.ShortDescription;
-        travelEl.append(shortDescriptionEl);
+    const shortDescriptionEl = document.createElement('p');
+    shortDescriptionEl.classList.add('karelia__travel_shortDescription');
+    shortDescriptionEl.textContent = travel.ShortDescription;
+    travelEl.append(shortDescriptionEl);
 
 });
 
+
 // Дни сплава на байдарках, splavChirkaKem
-const splavChirkaKem =  `[
+const splavChirkaKem = `[
     {
         "id": "1",
         "DayNumber": "1",
         "Photo": "./images/karelia1.jpg",
         "Location": "урочище Андронова Гора",
         "ShortDescription": "Наша отправная точка, собираем катамараны.",
-        "Description": "lorem50"
+        "Description": "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum architecto, tenetur, ullam esse eum obcaecati nesciunt laboriosam velit doloremque cum quaerat exercitationem quibusdam laudantium omnis explicabo blanditiis voluptate perferendis nisi. Consectetur pariatur asperiores, esse quos iste debitis, error labore quo culpa beatae numquam, animi"
     },
     {
         "id": "2",
@@ -110,39 +111,80 @@ const splavChirkaKem =  `[
 ]`
 const splavChirkaKemDays = JSON.parse(splavChirkaKem);
 
-splavChirkaKemDays.forEach(day => {
+const daysEl = document.querySelector('.karelia__splavChirkaKem-days');
 
-    const daysEl = document.querySelector('.karelia__splavChirkaKem-days');
+splavChirkaKemDays.forEach(day => {
 
     const dayEl = document.createElement('div');
     dayEl.classList.add('karelia__splavChirkaKem-day');
     daysEl.append(dayEl);
 
-        const dayNumberEl = document.createElement('p');
-        dayNumberEl.classList.add('karelia__splavChirkaKem-day_number');
-        dayNumberEl.textContent = `День ${day.DayNumber}`;
-        dayEl.append(dayNumberEl);
+    const dayNumberEl = document.createElement('p');
+    dayNumberEl.classList.add('karelia__splavChirkaKem-day_number');
+    dayNumberEl.textContent = `День ${day.DayNumber}`;
+    dayEl.append(dayNumberEl);
 
-        const dayPhotoEl = document.createElement('img');
-        dayPhotoEl.classList.add('karelia__splavChirkaKem-day_photo');
-        dayPhotoEl.src = day.Photo;
-        dayPhotoEl.setAttribute('alt', `photo-day${day.DayNumber}`);
-        dayEl.append(dayPhotoEl);
+    const dayLinkEl = document.createElement('a');
+    dayLinkEl.classList.add('karelia__splavChirkaKem-day_link');
+    dayLinkEl.setAttribute('href', `#karelia__splavChirkaKem-days`);
+    dayEl.append(dayLinkEl);
 
-        const locationEl = document.createElement('p');
-        locationEl.classList.add('karelia__splavChirkaKem-day_location');
-        locationEl.textContent = day.Location;
-        dayEl.append(locationEl);
+    const dayPhotoEl = document.createElement('img');
+    dayPhotoEl.classList.add('karelia__splavChirkaKem-day_photo');
+    dayPhotoEl.src = day.Photo;
+    dayPhotoEl.setAttribute('alt', `photo-day${day.DayNumber}`);
+    dayLinkEl.append(dayPhotoEl);
 
-        const shortDescriptionEl = document.createElement('p');
-        shortDescriptionEl.classList.add('karelia__splavChirkaKem-day_shortDescription');
-        shortDescriptionEl.textContent = day.ShortDescription;
-        dayEl.append(shortDescriptionEl);
+    const locationEl = document.createElement('p');
+    locationEl.classList.add('karelia__splavChirkaKem-day_location');
+    locationEl.textContent = day.Location;
+    dayEl.append(locationEl);
 
-    });
+    const shortDescriptionEl = document.createElement('p');
+    shortDescriptionEl.classList.add('karelia__splavChirkaKem-day_shortDescription');
+    shortDescriptionEl.textContent = day.ShortDescription;
+    dayEl.append(shortDescriptionEl);
+
+});
+
+//Клики по картинкам сплав на байдарках, река Чирка-Кемь
+
+daysEl.addEventListener('click', ({ target }) => {
+    if (target.classList.contains('karelia__splavChirkaKem-day_photo')) {
+        splavChirkaKemDays.forEach(day => {
+            console.log(day.id, Number(target.alt.at(-1)));
+            if (Number(day.id) === Number(target.alt.at(-1))) {
+                console.log('внутри');
+                const activeEl = document.createElement('div');
+                activeEl.classList.add('currentActive');
+                daysEl.append(activeEl);
+
+                const activePhotoEl = document.createElement('img');
+                activePhotoEl.classList.add('currentActive__photo');
+                activePhotoEl.src = day.Photo;
+                activePhotoEl.setAttribute('alt', `photo-day${day.DayNumber}`);
+                activeEl.append(activePhotoEl);
+
+                const activeDescriptionEl = document.createElement('p');
+                activeDescriptionEl.classList.add('currentActive__description');
+                activeDescriptionEl.textContent = day.Description;
+                activeEl.append(activeDescriptionEl);
+
+                const activeCloseEl = document.createElement('button');
+                activeCloseEl.classList.add('currentActive__close');
+                activeCloseEl.textContent = 'X';
+                activeEl.append(activeCloseEl);
+            }
+        });
+    }
+
+    if (target.classList.contains('currentActive__close')) {
+        target.parentElement.classList.add('currentActive__hidden');
+    }
+});
 
 // Город Кемь, kem
-const kem =  `[
+const kem = `[
     {
         "id": "1",
         "Photo": "./images/katun1.jpg",
@@ -174,19 +216,19 @@ kemPlaces.forEach(place => {
 
     const placesEl = document.querySelector('.karelia__kem-places');
 
-        const placeEl = document.createElement('div');
-        placeEl.classList.add('karelia__kem-place');
-        placesEl.append(placeEl);
+    const placeEl = document.createElement('div');
+    placeEl.classList.add('karelia__kem-place');
+    placesEl.append(placeEl);
 
-        const placePhotoEl = document.createElement('img');
-        placePhotoEl.classList.add('karelia__kem-place_photo');
-        placePhotoEl.src = place.Photo;
-        placePhotoEl.setAttribute('alt', `photo-place${place.id}`);
-        placeEl.append(placePhotoEl);
+    const placePhotoEl = document.createElement('img');
+    placePhotoEl.classList.add('karelia__kem-place_photo');
+    placePhotoEl.src = place.Photo;
+    placePhotoEl.setAttribute('alt', `photo-place${place.id}`);
+    placeEl.append(placePhotoEl);
 
-        const shortDescriptionEl = document.createElement('p');
-        shortDescriptionEl.classList.add('karelia__kem-place_shortDescription');
-        shortDescriptionEl.textContent = place.ShortDescription;
-        placeEl.append(shortDescriptionEl);
+    const shortDescriptionEl = document.createElement('p');
+    shortDescriptionEl.classList.add('karelia__kem-place_shortDescription');
+    shortDescriptionEl.textContent = place.ShortDescription;
+    placeEl.append(shortDescriptionEl);
 
-    });
+});
