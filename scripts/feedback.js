7602acbce5dd0674ff01a29bd63745296ffd4421
Denis Tkachev <allpost-s@ya.ru>
2024-05-08 14:21:07 +0300

    comments

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -1,45 +1,61 @@
 /**
  * Обработка клика по кнопке "Добавить отзыв"
  */
-
+// localStorage.clear();
 const travelNameEl = document.querySelector('.altai__horseTravel');
 
 const travelsLsKey = 'travels';
 const travelFeedbackEl = document.querySelector('.feedbackTravel__feedback');
 const saveFeedbackEl = document.querySelector('.feedbackTravel__saveFeedback');
 
-// if (localStorage.getItem(scheduleKey)) {
-//     localStorage.setItem(scheduleKey, JSON.stringify(scheduleData));
-// };
-
+const setToLocalStorage = (travelsFeedbacks) => {
+    localStorage.setItem(travelsLsKey, JSON.stringify(travelsFeedbacks));
+}
 
 // сохранение данных
 saveFeedbackEl.addEventListener('click', () => {
+
     const travel = travelNameEl.id;
     const feedback = travelFeedbackEl.value;
 
     if (localStorage.getItem(travelsLsKey) === null) {
-        const travelFeedbacks = [];
-        travelFeedbacks.push(feedback)
-        const travelFbList = [
-            {
-                travel,
-                travelFeedbacks
-            }
-        ];
-        localStorage.setItem(travelsLsKey, JSON.stringify(travelFeedbacks));
-    } else {
-        const travelFeedbacks = [];
-        const travelFbList = [];
 
-        JSON.parse(localStorage.getItem(travelsLsKey)).forEach(element => {
-            if (element.key === travel) {
-                
+        const travelsFeedbacks = []; //массив для всех отзывов по всем путешествиям
+        const feedbacks = []; // массив отзывов по конкретному путешествию
+        let travelFeedbacks = {}; // объект: путешествие, значение массив отзывов (feedbacks)
+        
+        feedbacks.push(feedback); // добавляем отзыв в массив отзывов
+        travelFeedbacks[travel] = feedbacks; // массив отзывов в объект по значению travel
+        travelsFeedbacks.push(travelFeedbacks); // добавляем объект с отзывами на путешествие в массив для всех отзывов и путешествий
+
+        setToLocalStorage(travelsFeedbacks);
+
+    } else {
+        const travelsFeedbacks = []; //массив для всех отзывов по всем путешествиям
+        const feedbacks = []; // массив отзывов по конкретному путешествию
+
+        JSON.parse(localStorage.getItem(travelsLsKey)).forEach(el => {
+            if (el[travel]) {
+                el[travel].forEach(el => {
+                    feedbacks.push(el);
+                }); 
             }
-            travelFbList.push({travel, element});
         });
 
-        travelFbList.push(feedback);
-        localStorage.setItem(travelsLsKey, JSON.stringify(travelFbList));
+        feedbacks.push(feedback);
+
+        JSON.parse(localStorage.getItem(travelsLsKey)).forEach(el => {
+            travelsFeedbacks.push(el);
+        });
+
+        travelsFeedbacks.forEach((el) => {
+            if (el[travel]) {
+                el[travel] = feedbacks; // добавляем в объект путешествие с массивом отзывов
+            }
+        });
+
+        setToLocalStorage(travelsFeedbacks);
     }
+
+    travelFeedbackEl.value = '';
 });
