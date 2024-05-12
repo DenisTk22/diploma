7a8437bb79d7a6b67b160ed57406a790eb172636
Denis Tkachev <allpost-s@ya.ru>
2024-05-12 22:55:41 +0300

    feedbacks

--- scripts/feedback.js
+++ scripts/feedback.js
@@ -5,15 +5,20 @@ localStorage.clear();
 const travelsLsKey = 'travels';
 
 // const travelNameEl = document.querySelector('.altai__horseTravel');
-// const traveslNameEl = document.querySelectorAll('.stayYourFeedback');
+const traveslNameEl = document.querySelectorAll('.stayYourFeedback');
 
 // const feedbackOfTravels = document.querySelectorAll('feedbackTravel');
 const contentEl = document.querySelector('.content');
 
 const travelFeedbackEl = document.querySelector('.feedbackTravel__feedback');
 const saveFeedbackEl = document.querySelector('.feedbackTravel__saveFeedback');
+
 const showFeedbackEl = document.querySelector('.feedbackTravel__showFeedbacks');
+const showFeedbackButtonsEl = document.querySelectorAll('.feedbackTravel__showFeedbacks');
+
 const listFeedbacks = document.querySelector('.feedbackTravel__listFeedback');
+const listOfFeedbacks = document.querySelectorAll('.feedbackTravel__listFeedback');
+
 let countOfClicks = 0;
 
 const setToLocalStorage = (travelsFeedbacks) => {
@@ -22,27 +27,35 @@ const setToLocalStorage = (travelsFeedbacks) => {
 
 const showFeedbacks = () => {
     if (localStorage.getItem(travelsLsKey) !== null) {
-        const travel = travelNameEl.id;
-        JSON.parse(localStorage.getItem(travelsLsKey)).forEach(el => {
-            if (el[travel] && el[travel].length !== 0) {
-                countOfClicks++;
-                showFeedbackEl.disabled = false;
-                showFeedbackEl.textContent = 'Показать отзывы';
-            } else {
-                emptyFeedbacks();
-            }
-        });
+        traveslNameEl.forEach(el => {
+            const travel = el.id;
+            const travelName = el;
+            JSON.parse(localStorage.getItem(travelsLsKey)).forEach(el => {
+                if (el[travel] && el[travel].length !== 0) {
+                    countOfClicks++;
+                    const showFeedbackButton = travelName.querySelector('.feedbackTravel__showFeedbacks');
+                    showFeedbackButton.disabled = false;
+                    showFeedbackButton.textContent = 'Показать отзывы';
+                } else {
+                    emptyFeedbacks(travelName);
+                }
+            });
+        })
     }
 }
 
-const emptyFeedbacks = () => {
+const emptyFeedbacks = (parent) => {
+    const listEl = parent.querySelector('.feedbackTravel__listFeedback');
     const feedbackEl = document.createElement('li');
     feedbackEl.textContent = 'Отзывов пока нет';
-    listFeedbacks.append(feedbackEl);
+    listEl.append(feedbackEl);
 }
 
 const renderFeedBacks = (travel) => {
-    listFeedbacks.innerHTML = ""; // удаление списка предыдущих отзывов
+
+    const parent = document.getElementById(`${travel}`);
+    const listEl = parent.querySelector('.feedbackTravel__listFeedback');
+    listEl.innerHTML = ""; // удаление списка предыдущих отзывов
     JSON.parse(localStorage.getItem(travelsLsKey)).forEach(el => {
         let presentFeedbacks = false;
         if (el[travel] && el[travel].length !== 0) {
@@ -54,19 +67,19 @@ const renderFeedBacks = (travel) => {
 
                 feedbackEl.textContent = el;
                 feedbackEl.setAttribute('data-local-id', `${i}`)
-                listFeedbacks.append(feedbackEl);
+                listEl.append(feedbackEl);
 
                 removeFeedbackEl.classList.add('feedbackTravel__removeFeedback');
                 removeFeedbackEl.textContent = 'Удалить отзыв';
                 feedbackEl.append(removeFeedbackEl);
-                
+
                 editFeedbackEl.classList.add('feedbackTravel__editFeedback');
                 editFeedbackEl.textContent = 'Редактировать отзыв';
                 feedbackEl.append(editFeedbackEl);
             });
         }
         if (!presentFeedbacks) {
-            emptyFeedbacks();
+            emptyFeedbacks(parent);
         }
     });
 }
@@ -77,52 +90,63 @@ showFeedbacks();
 // Добавление отзыва
 contentEl.addEventListener('click', (e) => { //saveFeedbackEl
     e.preventDefault();
-    if (travelFeedbackEl.value !== '') {
-        // const travel = travelNameEl.id;
+    if (e.target.classList.contains('feedbackTravel__saveFeedback')) {
         const travel = e.target.parentElement.parentElement.id;
-        const feedback = travelFeedbackEl.value;
-        showFeedbackEl.disabled = false;
 
-        if (localStorage.getItem(travelsLsKey) === null) {
+        const travelName = e.target.parentElement.parentElement;
+        const travelFeedbackField = travelName.querySelector('.feedbackTravel__feedback');
 
-            const travelsFeedbacks = []; //массив для всех отзывов по всем путешествиям
-            const feedbacks = []; // массив отзывов по конкретному путешествию
-            let travelFeedbacks = {}; // объект: путешествие, значение массив отзывов (feedbacks)
+        if (travelFeedbackField.value !== '') {
 
-            feedbacks.push(feedback); // добавляем отзыв в массив отзывов
-            travelFeedbacks[travel] = feedbacks; // массив отзывов в объект по значению travel
-            travelsFeedbacks.push(travelFeedbacks); // добавляем объект с отзывами на путешествие в массив для всех отзывов и путешествий
+            const feedback = travelFeedbackField.value;
 
-            setToLocalStorage(travelsFeedbacks);
-        } else {
-            const travelsFeedbacks = []; //массив для всех отзывов по всем путешествиям
-            const feedbacks = []; // массив отзывов по конкретному путешествию
+            const showFeedbackButton = travelName.querySelector('.feedbackTravel__showFeedbacks');
+            showFeedbackButton.disabled = false;
 
-            JSON.parse(localStorage.getItem(travelsLsKey)).forEach(el => {
-                if (el[travel]) {
-                    el[travel].forEach(el => {
-                        feedbacks.push(el);
-                    });
-                }
-            });
 
-            feedbacks.push(feedback);
+            if (!localStorage.getItem(travelsLsKey)) {
+                const travelsFeedbacks = []; //массив для всех отзывов по всем путешествиям
+                const feedbacks = []; // массив отзывов по конкретному путешествию
+                let travelFeedbacks = {}; // объект: путешествие, значение массив отзывов (feedbacks)
 
-            JSON.parse(localStorage.getItem(travelsLsKey)).forEach(el => {
-                travelsFeedbacks.push(el);
-            });
+                feedbacks.push(feedback); // добавляем отзыв в массив отзывов
+                travelFeedbacks[travel] = feedbacks; // массив отзывов в объект по значению travel
+                travelsFeedbacks.push(travelFeedbacks); // добавляем объект с отзывами на путешествие в массив для всех отзывов и путешествий
 
-            travelsFeedbacks.forEach((el) => {
-                if (el[travel]) {
-                    el[travel] = feedbacks; // добавляем в объект путешествие с массивом отзывов
-                }
-            });
+                setToLocalStorage(travelsFeedbacks);
+            }
+            
+            else {
 
-            setToLocalStorage(travelsFeedbacks);
+                const travelsFeedbacks = []; //массив для всех отзывов по всем путешествиям
+                const feedbacks = []; // массив отзывов по конкретному путешествию
+                let travelFeedbacks = {}; // объект: путешествие, значение массив отзывов (feedbacks)
+
+                JSON.parse(localStorage.getItem(travelsLsKey)).forEach(el => {
+                    travelsFeedbacks.push(el);
+                });   
+                let present = false;
+
+
+                travelsFeedbacks.forEach((el) => {
+                    if (el[travel]) {
+                        present = true;
+                        el[travel].push(feedback); // добавляем в объект путешествие с массивом отзывов
+                    } });
+
+                    if (!present) 
+                     {
+                        feedbacks.push(feedback);
+                        travelFeedbacks[travel] = feedbacks;
+                        travelsFeedbacks.push(travelFeedbacks);
+                    }
+
+                setToLocalStorage(travelsFeedbacks);
+            }
+            renderFeedBacks(travel);
         }
-       renderFeedBacks(travel); 
+        travelFeedbackField.value = '';
     }
-    travelFeedbackEl.value = '';
 });
 
 // вывод отзывов на страницу и скрытие отзывов со страницы
